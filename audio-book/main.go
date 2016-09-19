3f05e00e49029dce99243e5ed1567aaf599ab515
huydv0109 <huydv0109@gmail.com>
2016-09-19 10:51:25 +0700

    first commit

---
+++ audio-book/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@/gotest")
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer db.Close()
+	// make sure connection is available
+	err = db.Ping()
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	type Person struct {
+		Id         int
+		First_Name string
+		Last_Name  string
+	}
+	router := gin.Default()
+	// Add API handlers here
+
+	router.GET("/persons", func(c *gin.Context) {
+		var (
+			person  Person
+			persons []Person
+		)
+		rows, err := db.Query("select id, first_name, last_name from person;")
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		for rows.Next() {
+			err = rows.Scan(&person.Id, &person.First_Name, &person.Last_Name)
+			persons = append(persons, person)
+			if err != nil {
+				fmt.Print(err.Error())
+			}
+		}
+		defer rows.Close()
+		c.JSON(http.StatusOK, gin.H{
+			"result": persons,
+			"count":  len(persons),
+		})
+	})
+
+	router.Run(":3000")
+}
